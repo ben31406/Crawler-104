5206e479e34f1ff888f04361def9620b9fd88594
Ben <ben02153456@gmail.com>
2021-07-14 17:28:47 +0800

    Fix no search result bug

--- Crawler_104/setting.py
+++ Crawler_104/setting.py
@@ -1,8 +1,7 @@
 # list： job keywords
 # example： ['前端工程師', '軟體工程師']
-JOB_KEY = ['數據處理', '數據分析', '數據應用', '資料處理', '資料分析', '資料應用','商業分析','爬蟲', '爬網',
-           'Data Analyst', '資料管理分析', '商業智慧', '商業智能', 'Business Intelligence']
+JOB_KEY = []
 
 # 檔案要輸出的路徑(不需要檔名，只需要路徑)
-SAVE_PATH = r'C:\Users\Ben\Desktop\104工作搜尋'
+SAVE_PATH = r''
 
